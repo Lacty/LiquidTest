7417e5c7d59878a9da5630a0803e46bb2109e412
Lacty <akira206@gmail.com>
2015-11-01 20:09:14 +0900

    Boxの生成位置を変更できた

--- tests_my/test_drawBox.js
+++ tests_my/test_drawBox.js
@@ -1,30 +1,23 @@
 function TestDrawBox() {
   // setup ground
   var body = new b2BodyDef();
-  var ground = world.CreateBody(body);
+  var ground = world.CreateBody(body);  // １度目
   
   // Edge生成
   var edge = new b2EdgeShape();
+  // 始点、終点を指定
   edge.Set(new b2Vec2(-20, 0), new b2Vec2(20, 0));
+  ground.CreateFixtureFromShape(edge, 0);
   
-  var fd = new b2FixtureDef();
-  fd.shape = edge;
-  ground.CreateFixtureFromDef(fd);
-  
-  /*
-   * 以下でもEdgeを描画できる模様
-   * 違いは不明
-   * var edge = new b2EdgeShape();
-   * edge.Set(new b2Vec2(-20, 0), new b2Vec2(20, 0));
-   * ground.CreateFixtureFromShape(edge, 0);
-   */
   
   // Box生成
   var box = new b2PolygonShape();
   // サイズを指定
   box.SetAsBoxXY(2, 2);
-  
-  fd = new b2FixtureDef();
-  fd.shape = box;
-  ground.CreateFixtureFromDef(fd);
+  // 位置を指定
+  body.position.Set(0, 5);
+
+  ground = world.CreateBody(body);       // ２度目
+  ground.CreateFixtureFromShape(box, 0); // 第２引数が何を示しているか不明
+                                         // 今のところ何を指定しても変化なし
 }
